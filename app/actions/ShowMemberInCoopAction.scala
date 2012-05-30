e56b099c38a3f45ce67eae9aff0af76bcbc43490
joshua calloway <joshua.calloway@gmail.com>
2012-05-30 02:16:21 -0500

    new Action

---
+++ app/actions/ShowMemberInCoopAction.scala
@@ -0,0 +1,33 @@
+package actions
+
+import anorm._
+import models.Coop
+import models.Member
+import play.api.Play.current
+import play.api._
+import play.api.data.Forms._
+import play.api.data._
+import play.api.mvc._
+import views._
+
+object ShowMemberInCoopAction extends Controller with Secured {
+  // val form: Form[Member] = Form(
+  //   mapping(
+  //     "id" -> ignored(NotAssigned:Pk[Long]),
+  //     "name" -> nonEmptyText,
+  //     "email" -> optional(text),
+  //     "password" -> nonEmptyText,
+  //     "cell" -> optional(text),
+  //     "address" -> text)(Member.apply)(Member.unapply))
+
+  def showItem(coopId: Long, memberId: Long) : play.api.mvc.Action[play.api.mvc.AnyContent] =
+    Action {
+      val coopOpt = Coop.findById(coopId)
+      val memberOpt = Member.findById(memberId)
+      (coopOpt, memberOpt) match {
+        case (Some(coop), Some(member)) if coop.isMember(member) => Ok("member exists")
+        case _ => Ok("member does not belong in coop")
+      }
+    }    
+
+}
